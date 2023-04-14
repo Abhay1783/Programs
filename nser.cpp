b34e84f6ee76e05550fffc8fe66f4aa194104023
Abhay1783 <118812517+Abhay1783@users.noreply.github.com>
2023-04-14 09:20:35 +0530

    Add files via upload

---
+++ nser.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int next_smaller_to_right(vector<int>& arr) {
+    int result(arr.size(), -1);
+    stack<int> s;
+    for(int i = arr.size() - 1; i >= 0; i--) {
+        while(!s.empty() && s.top() >= arr[i]) {
+            s.pop();
+        }
+        if(!s.empty()) {
+            result[i] = s.top();
+        }
+        s.push(arr[i]);
+    }
+    return result;
+}
+
+int main() {
+    arr = {3, 5, 2, 4, 6, 1};
+    result = next_smaller_to_right(arr);
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl; 
+    return 0;
+}
+
